b2258bcb88e5e7a1ec34beb2222dea0c05290f35
SlitMagazine <submit2slit@gmail.com>
2026-09-18 10:01:25 -0500

    SLIT!

---
+++ button.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('hideBtn');
+const targetDiv = document.getElementById('popUp');
+
+button.addEventListener('click', function() {
+targetDiv.style.display = 'none';
+});
